28dd66f5dad7b05bcb695a7a4a259c69ddf2a91f
Ivan Aleinikov <alejnikov99.99@mail.ru>
2023-12-04 21:21:17 +0300

    three exercise

--- main.py
+++ main.py
@@ -119,14 +119,70 @@ cool_student.courses_in_progress += ['Python']
 cool_student.rate_lr(best_lecturer, 'Python', 10)
 cool_student.rate_lr(best_lecturer, 'Python', 10)
 
+# Задание 4
+
+student1 = Student('Alice', 'Johnson', 'female')
+student1.courses_in_progress = ['Python', 'OOP']
+student2 = Student('Bob', 'Smith', 'male')
+student2.courses_in_progress = ['Python', 'Git']
+
+lecturer1 = Lecturer('Jim', 'Brown')
+lecturer1.courses_attached = ['Python', 'OOP']
+lecturer2 = Lecturer('Robin', 'Dylan')
+lecturer2.courses_attached = ['Python', 'Git']
+
+reviewer1 = Reviewer('Sam', 'Green')
+reviewer1.courses_attached = ['Python', 'OOP']
+reviewer2 = Reviewer('Sara', 'Lopes')
+reviewer2.courses_attached = ['Python', 'Git']
+
+reviewer1.rate_hw(student1, 'Python', 9)
+reviewer1.rate_hw(student1, 'Python', 8)
+reviewer2.rate_hw(student2, 'OOP', 7)
+reviewer2.rate_hw(student2, 'OOP', 6)
+
+student1.rate_lr(lecturer1, 'Python', 10)
+student2.rate_lr(lecturer2, 'OOP', 9)
+student1.rate_lr(lecturer1, 'Git', 8)
+student2.rate_lr(lecturer2, 'OOP', 7)
 
 
-print(best_student.grades)
+print("Student 1:")
+print()
+print("Student 2:")
+print()
+print("Lecturer 1:")
+print()
+print("Lecturer 2:")
+print()
+print("Reviewer 1:")
+print()
+print("Reviewer 2:")
+print()
 
-print(best_student.grades)
 
-print(cool_reviewer)
+lst = [student1, student2]
 
-print(best_lecturer)
+def awg(students, course):
+    s = []
+    for i in students:
+        for key, value in i.grades.items():
+            if key == course:
+                for j in value:
+                    s.append(j)
+    return f'Средняя оценка студентов по предмету {course}: {sum(s) / len(s)}'
 
-print(best_student)
+print(awg(lst, 'Python'))
+
+lst1 = [lecturer1, lecturer2]
+
+def awg_lec(lecturer, course):
+    s = []
+    for i in lecturer:
+        for key, value in i.grades_lecturer.items():
+            if key == course:
+                for j in value:
+                    s.append(j)
+    return f'Средняя оценка лекторов по предмету {course}: {sum(s) / len(s)}'
+
+print(awg_lec(lst1, 'Git'))
